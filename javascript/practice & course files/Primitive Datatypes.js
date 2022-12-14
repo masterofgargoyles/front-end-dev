ef8fc942197f7a7bea1de143f0152af9b03e093c
Yudhajit Chowdhury <110385709+masterofgargoyles@users.noreply.github.com>
2022-12-15 00:01:55 +0530

    commit

---
+++ javascript/practice & course files/Primitive Datatypes.js
@@ -0,0 +1,12 @@
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("567");
+let e = "Harry";
+let f = Symbol("I am a symbol");
+let g
+
+const item = {
+  "harry": true,
+  "subh": false
+};

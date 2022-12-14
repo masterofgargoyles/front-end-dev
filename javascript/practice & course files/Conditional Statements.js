6f0ed242240ec85151ed5e220741fe03b8566dd5
Yudhajit Chowdhury <110385709+masterofgargoyles@users.noreply.github.com>
2022-12-15 00:01:42 +0530

    commit

---
+++ javascript/practice & course files/Conditional Statements.js
@@ -0,0 +1,9 @@
+let a = prompt("Enter yourr age");
+a = Number.parseInt(a);
+
+if(a>=20){
+  console.log("You can drive");
+}
+else{
+  console.log("You cannot drive");
+}
---
+++ javascript/practice & course files/var_let_const.js
@@ -0,0 +1,12 @@
+var a = 45;
+var b = "harry"
+var c = null;
+
+const harry = 0;
+
+{
+  let b = 'this';
+  console.log(b);
+}
+
+console.log(b);

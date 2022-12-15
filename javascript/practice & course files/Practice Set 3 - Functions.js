cf79023563b769ebd49257b14e8d94e7436a9100
Yudhajit Chowdhury <110385709+masterofgargoyles@users.noreply.github.com>
2022-12-15 23:02:41 +0530

    commit

---
+++ javascript/practice & course files/Practice Set 3 - Functions.js
@@ -0,0 +1,30 @@
+
+// let obj = {
+//   harry: 98,
+//   rohan: 70,
+//   akash: 73
+// };
+// let i = 0;
+// for (i = 0; i < Object.keys(obj).length; i++) {
+//   console.log(Object.keys(obj)[i] + " " + obj[Object.keys(obj)[i]]);
+// }
+
+// for (key in obj) {
+//   console.log(key + " " + obj[key]);
+// }
+
+// let cn = 4;
+// let i;
+// while(i!=cn){
+//   i = prompt("Enter the correct number");
+// }
+// console.log("You have entered the correct number");
+
+const mean = ()=>{
+  return (1+2+3+4)/4;
+}
+
+console.log(mean());
+
+
+
